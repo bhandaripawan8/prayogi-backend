16b8b179f74ee26bb7b98149329863852b10a8d4
Pawan <pawanb78@gmail.com>
2024-06-15 00:11:20 +0100

    crud yoga classes tested with postman successfully

--- controllers/yogaClassesController.js
+++ controllers/yogaClassesController.js
@@ -9,7 +9,7 @@ const router = express.Router();
 export const createNewClass = async (req, res) => {
     try {
         const { image, header, description, days, time } = req.body;
-        const existingClass = await YogaClasses.findOne({ header, user });
+        const existingClass = await YogaClasses.findOne({ header });
         if (existingClass) {
           return res.status(400).json({ message: 'Duplicate yoga class found, please create a different one', success: false });
         }    
@@ -48,8 +48,8 @@ export const getOneYogaClass = async (req, res) => {
 // UPDATE - PUT /api/yoga-classes/:id
 export const updateYogaClass = async (req, res) => {
     try {
-        const { image, header, description, days, time } = req.body;
-        const updatedClass = await YogaClasses.findByIdAndUpdate(req.params.id, { image, header, description, days, time }, { new: true });
+        const { image, header, description, days, time, user } = req.body;
+        const updatedClass = await YogaClasses.findByIdAndUpdate(req.params.id, { image, header, description, days, time, user }, { new: true });
         if (updatedClass) {
             res.json({message: 'Class updated successfully', updatedClass});
         } else {
